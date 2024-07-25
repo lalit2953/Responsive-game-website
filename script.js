a9cecdc0cb273fb43f85dd3ec2018f35f08fad57
Lalit Jagtap <lalitjagtap6603@gmail.com>
2024-07-25 21:39:05 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+@media(max-width:1740px){
+    header{
+        padding: 15px 4%;
+    }
+    section{
+        padding: 0 10px;
+
+    }
+    .social-icons{
+        padding: 0 4%;
+
+    }
+    .images-box{
+          right: 4%;
+    }
+    .scroll-btn{ 
+          left: 4%;
+          
+    }
+}
+@media(max-width:1460px){
+    :root{
+        --h1-font: 4.5;
+    }
+}
+@media(max-width:1240px){
+    .social-icons{
+        display: none;
+    }
+    section{
+
+        padding: 0 4%;
+    }
+}
+
+@media(max-width:1050px){
+    .scroll-btn{
+        display: none;
+
+    }
+    .images-box{
+        display: none;
+    }
+    .logo{
+        font-size: 26px;
+    }
+    .hero{
+        height: 100px;
+        display: flex;
+        flex-wrap: wrap;
+        gap:2rem;
+    }
+}
